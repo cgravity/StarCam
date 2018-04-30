b41bbde90d6437cb8fa1faf2ccda118a966974e5
StarCAM Development PC <chei@sensei-box.localdomain>
2018-04-30 13:36:48 -0700

    drain.py -- fixes from testing on system

--- drain.py
+++ drain.py
@@ -1,41 +1,45 @@
-#!python2
+#!/usr/bin/python
 
 import os
 from multiprocessing import Pool
 import datetime
 from shutil import copyfile
 
-drives = list("CDFGHIJ")
+drives = list("cdfghij")
 cams = [str(i) for i in xrange(16)]
 
-output_base_path = "C:\\sensei-data\\"
+output_base_path = "/mnt/c/sensei-data/"
 
 now = datetime.datetime.now().isoformat().replace(':','-')
 
 def copy(from_drive):
     for cam in cams:
-        src_path = os.path.join(from_drive, "test", cam)
+        src_path = os.path.join("/mnt", from_drive, "test", cam)
         dst_path = os.path.join(output_base_path, now, cam)
         
-        if not os.path.exists(dst_path)
+	print "[", from_drive, cam, "] copy", src_path, "->", dst_path
+
+        if not os.path.exists(dst_path):
             os.makedirs(dst_path)
         
         for entry in os.listdir(src_path):
             src_file = os.path.join(src_path, entry)
             dst_file = os.path.join(dst_path, entry)
-            copyfile(src, dst)
+            copyfile(src_file, dst_file)
 
 def clean(from_drive):
     for cam in cams:
-        src_path = os.path.join(from_drive, "test", cam)
+        src_path = os.path.join("/mnt", from_drive, "test", cam)
         
         for entry in os.listdir(src_path):
-            os.remove(entry)
+            full_path_entry = os.path.join(src_path, entry)
+            print "remove:", full_path_entry
+            os.remove(full_path_entry)
 
 def drain(from_drive):
     copy(from_drive)
     clean(from_drive)
 
 pool = Pool(8)
-p.map(drain, drives)
+pool.map(drain, drives)
 
